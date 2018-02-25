95de50fa7ee9dad963b91e3a7a41ed3d7d1503ec
Llhei <vyhnland@gmail.com>
2018-02-25 13:24:38 +0800

    fixed embeds

--- events/message.js
+++ events/message.js
@@ -3,12 +3,15 @@ exports.run = (client, message) => {
   //pokecord listener
 
   if(message.author.id === "365975655608745985") {
-    if(message.embeds.title.indexOf("A wild pokémon has appeared")) {
+    if(message.embeds !== 'undefined' && message.embeds.length > 0) {
+      if(message.embeds[0].title.indexOf("A wild pokémon has appeared")) {
 
-      let pokecordRole = "<@&417180976708452352>";
-      message.channel.send(pokecordRole);
+        let pokecordRole = "<@&417180976708452352>";
+        message.channel.send(pokecordRole);
 
+      }
     }
+
 
 
   }
