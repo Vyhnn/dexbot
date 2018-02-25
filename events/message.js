24b8bf0c89e2cb1bc97b46360b705b105343759d
Llhei <vyhnland@gmail.com>
2018-02-25 12:19:14 +0800

    fixed pokecord listener

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ exports.run = (client, message) => {
 
   //pokecord listener
 
-  if(message.author.id === "175984908802457600") {
+  if(message.author.id === "365975655608745985") {
     if(message.embeds.title.indexOf("A wild pokémon has appeared")) {
       console.log("A wild pokemon appeared");
     }
