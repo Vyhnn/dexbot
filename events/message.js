78f724408491e56f2fbaa0b965a89dc0a9fa586c
Llhei <vyhnland@gmail.com>
2018-02-25 13:32:31 +0800

    pokecord addict

--- events/message.js
+++ events/message.js
@@ -4,10 +4,13 @@ exports.run = (client, message) => {
 
   if(message.author.id === "365975655608745985") {
     if(message.embeds !== 'undefined' && message.embeds.length > 0) {
-      if(message.embeds[0].title.indexOf("A wild pokémon has appeared")) {
+        message.embeds.forEach(function(embed) {
+          if(embed.title==="A wild pokémon has appeared!") {
+            let pokecordRole = "<@&417180976708452352>";
+            message.channel.send(pokecordRole);
+          }
+        });
 
-        let pokecordRole = "<@&417180976708452352>";
-        message.channel.send(pokecordRole);
 
       }
     }
